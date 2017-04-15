22d8a3e4d5d875b274263bc890fdb0a998c26da1
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-15 18:58:22 +0200

    Add files via upload

---
+++ Maximum_Draws.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+//problem link : https://www.hackerrank.com/challenges/maximum-draws
+// Name        : Maximum_Draws.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status: accepted
+//============================================================================
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n+1<<endl;
+    }
+    return 0;
+}
