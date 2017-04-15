55fffa331f80530d1ef5cb2abaeef894359127a2
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-15 18:20:18 +0200

    Add files via upload

---
+++ ConditionalStatements.cpp
@@ -0,0 +1,45 @@
+//============================================================================
+//problem link :https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else
+// Name        : ConditionalStatements.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style.
+//status: accepted
+//============================================================================
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    if(n==1){
+        cout<<"one";
+         }
+    if(n==2)
+        cout<<"two";
+    if(n==3)
+        cout<<"three";
+    if(n==4)
+        cout<<"four";
+    if(n==5)
+        cout<<"five";
+    if(n==6)
+        cout<<"six";
+    if(n==7)
+        cout<<"seven";
+    if(n==8)
+        cout<<"eight";
+        if(n==9)
+        cout<<"nine";
+        if(n>9)
+        cout<<"Greater than 9";
+   return 0;
+}
