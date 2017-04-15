69027070c9ca6da9208654b846d74e66372374e5
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-15 15:56:01 +0200

    Add files via upload

---
+++ Hello_world_hackerrank.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+//problem link: https://www.hackerrank.com/challenges/30-hello-world
+// Name        : Hello_world_hackerrank.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status :accepted
+//
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   string inputString; // declare a variable to hold our input
+   getline(cin, inputString); // get a line of input from cin and save it to our variable
+
+
+   cout << "Hello, World." << endl;
+   cout<<inputString<<endl;
+
+
+
+   return 0;
+}
+
