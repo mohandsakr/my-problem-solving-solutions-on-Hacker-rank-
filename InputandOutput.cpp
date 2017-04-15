15af45f90e237b1c706c5b392b540166cea13072
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-15 16:53:58 +0200

    Add files via upload

---
+++ InputandOutput.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+//problem link :https://www.hackerrank.com/challenges/30-arrays
+// Name        : InputandOutput.cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status : accepted
+//============================================================================
+#include <iostream>
+using namespace std;
+int main() {
+int a,b,c;
+    int sum =0;
+    cin>>a>>b>>c;
+    sum =a+b+c;
+    cout<<sum;
+    return 0;
+}
